5499656836a23b93763b7bd6794493a189d195e0
xinDW <1320598835@qq.com>
2019-05-27 14:37:05 +0800

    feat: auto infer the input size

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import time
 
-from config import *
+from config import config
 from utils import *
 from model import UNet
 
@@ -20,7 +20,6 @@ def read_valid_images(path):
     for img_file in img_list:
         print(path + img_file)
         img = get_lf_extra(img_file, path) 
-        
 
         if (img.dtype != np.float32):
             img = img.astype(np.float32, casting='unsafe')
@@ -35,12 +34,49 @@ def read_valid_images(path):
     print('read %d from %s' % (len(img_set), path)) 
     img_set = np.asarray(img_set)
     print(img_set.shape)
-    return img_set
+    _, height, width, _ = img_set.shape.as_list()
+    
+    return img_set, height, width
 
-n_num = config.PSF.Nnum
+
 
 def evaluate(epoch, batch_size=1):
-    #checkpoint_dir = "checkpoint/"
+    '''
+    def _eval_images_in_folder_root(folder, check_empty=False, sess=None):  
+        valid_img_list = sorted(tl.files.load_file_list(path=folder, regx='.*.tif', printable=False))
+        valid_lf2d_imgs = read_all_imgs(valid_img_list, path=folder, type='2d', n_threads=batch_size, check_empty=check_empty)
+
+        if len(valid_lf2d_imgs) == 0: 
+            return 
+
+        save_dir_ = '{}/{}/'.format(save_dir, folder)  
+        tl.files.exists_or_mkdir(save_dir_)
+  
+        cell_value = np.zeros([batch_size, cell_size[0], cell_size[1], 512])
+        for idx in range(0, len(valid_img_list), batch_size):
+            image = tl.prepro.threading_data(valid_lf2d_imgs[idx : idx + batch_size], fn=lf_extract_fn, mode='toChannel', padding=False)
+            #image = get_img2d_fn(file, config.VALID.lf2d_path)
+            #image = lf_extract_fn(image, mode='toChannel', padding=False)
+            # print(image.shape)
+            start_t = time.time()
+            out, cell_value = sess.run([net.outputs, cell], {t_image: image, t_cell : cell_value})         
+            print('time elapsed : %4.4fs' % (time.time() - start_t))  
+            print("saving {}/{} ...".format(idx, len(valid_img_list)))
+            
+            write3d(out, save_dir_+'/{}_epoch{}_{:0>4}.tif'.format(tag, epoch, idx//batch_size))
+
+    def _eval_recursively(root):
+        print(root)
+        _eval_images_in_folder_root(root, check_empty=False)
+        
+        dirs = os.listdir(root)
+        for file in dirs:
+            child_path = os.path.join(root, file) + '/'
+            if os.path.isdir(child_path) and (not ('net_recon' in file)):
+                _eval_recursively(child_path)
+
+    '''
+    #checkpoint_dir = "checkpoint/bead_simu_resolution_test/"
 
     checkpoint_dir = config.TRAIN.ckpt_dir
     lf_size = config.VALID.lf2d_size
@@ -48,44 +84,56 @@ def evaluate(epoch, batch_size=1):
     save_dir = config.VALID.saving_path
     tl.files.exists_or_mkdir(save_dir)
     
-    start_time = time.time()
-    valid_lf_extras = read_valid_images(valid_lr_img_path)
+    n_num = config.PSF.Nnum
+    devices_num = config.TRAIN.device_num
+
     
-    t_image = tf.placeholder('float32', [batch_size, int(np.ceil(lf_size[0]/n_num)) , int(np.ceil(lf_size[1]/n_num)), n_num ** 2])
-    net = UNet(t_image, config.PSF.n_slices, lf_size, is_train=True, reuse=False, name='unet') 
+    valid_lf_extras, height, width = read_valid_images(valid_lr_img_path)
+    
+    #t_image = tf.placeholder('float32', [batch_size, int(np.ceil(lf_size[0]/n_num)) , int(np.ceil(lf_size[1]/n_num)), n_num ** 2])
+    t_image = tf.placeholder('float32', [batch_size, height , width, n_num ** 2])
+    with tf.device('/cpu:0'):
+    #with tf.device('/gpu:0'):
+        net = UNet(t_image, config.PSF.n_slices, lf_size, is_train=True, reuse=False, name='unet') 
   
     ckpt_found = False
 
     filelist = os.listdir(checkpoint_dir)
     for file in filelist:
         if '.npz' in file and str(epoch) in file:
-                ckpt_file = file
-                ckpt_found = True
-                break
+        #if '.npz' in file and str(epoch) and(config.label) in file:
+            ckpt_file = file
+            ckpt_found = True
+            break
 
     if not ckpt_found:
         raise Exception('no such checkpoint file')
-            
+
+          
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess:
         tl.layers.initialize_global_variables(sess)
         tl.files.load_and_assign_npz(sess=sess, name=checkpoint_dir + '/' + ckpt_file, network=net)
         
         for idx in range(0,len(valid_lf_extras), batch_size):
-            
+            start_time = time.time()  
             recon = sess.run(net.outputs, {t_image : valid_lf_extras[idx:idx+batch_size]})
-            write3d(recon, save_dir+'net_%s_%06d_epoch%d.tif' % (label, idx, epoch))
+            print("time elapsed : %4.4fs " % (time.time() - start_time))
+            write3d(recon, save_dir+'net_%s_%06d_epoch%d.tif' % (config.label, idx, epoch))
             #write3d(out, save_dir+'/epoch{}_{:0>4}.tif'.format(epoch, idx//batch_size))
             print('writing %d / %d ...' % (idx + 1, len(valid_lf_extras)))
 
           
-    print("time elapsed : %4.4fs " % (time.time() - start_time))
+    
  
 if __name__ == '__main__':
     import argparse
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ckpt', type=int, default=0)
+    parser.add_argument('-c', '--ckpt', type=int, default=0)
     parser.add_argument('--batch', type=int, default=1)
+    parser.add_argument("-r", "--recursive", help="recursively eval all images under config.VALID.lf2d_path and its sub-folders",
+                        action="store_true") #if the option is specified, assign True to args.recursive. Not specifying it implies False.
+    
     args = parser.parse_args()
     ckpt = args.ckpt
     batch_size = args.batch
