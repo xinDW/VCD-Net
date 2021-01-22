2424dc4d27bfd393d8f4b03a98df9371baf1fc16
xinDW <1320598835@qq.com>
2021-01-22 12:07:54 +0800

    fix: missing model UNet_B

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
-from .unet import UNet_A
+from .unet import *
+# from .unet_care import *
 # from .atrous_unet import AtrousUNet
 # from .rdn import RDN
 # from .split_atrous_unet import SplitAtrousUNet, MinimumUNet, MergeHead
